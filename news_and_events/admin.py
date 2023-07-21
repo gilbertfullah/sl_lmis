be3ddcf5d2fab38fbf8db02254810be7c0373c34
Gilbert Fullah <gilbertfullah@gmail.com>
2023-07-21 12:25:24 +0000

    new changes

--- news_and_events/admin.py
+++ news_and_events/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NewsAndEvents
+from .models import NewsAndEvents, Image
 
 class NewsAndEventsAdmin(admin.ModelAdmin):
     list_filter = ['title', 'tag', 'author']
