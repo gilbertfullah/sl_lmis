f240dac183c3158764b84a40d1e65da09ce3ddff
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-03 18:20:32 +0000

    new changes

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import DownloadDocument
 
-# Register your models here.
+class DownloadDocsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_per_page = 10
+
+    
+admin.site.register(DownloadDocument, DownloadDocsAdmin)
