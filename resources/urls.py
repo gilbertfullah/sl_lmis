6b665aaba340162fd7c7ba7833e362afb2024909
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-03 19:00:06 +0000

    new changes

--- resources/urls.py
+++ resources/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-     #path('resources/', views., name='resources'),
+     path('', views.resources, name='resources'),
      path('resources/resource_list/', views.resource_list, name='resource_list'),
 ]
--- resources/views.py
+++ resources/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import DownloadDocument
 
+def resources(request):
+    return render(request, 'resources.html')
+
 def resource_list(request):
     downloads = DownloadDocument.objects.all()
     context = {
