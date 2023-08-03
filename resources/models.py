eac1df41968302d72c5a49620ba1e37495af5ba3
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-03 18:25:59 +0000

    new changes

--- resources/models.py
+++ resources/models.py
@@ -4,7 +4,7 @@ class DownloadDocument(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(blank=True)
     document = models.FileField(upload_to='documents/')
-    download_url = models.URLField()
+    #download_url = models.URLField()
     uploaded_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
