1088b20d432001edde73a7bc8fa518df19b1eff8
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-03 23:15:22 +0000

    new changes

--- resources/urls.py
+++ resources/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-     path('', views.resources, name='resources'),
-     path('resources/resource_list/', views.resource_list, name='resource_list'),
+     #path('', views.resources, name='resources'),
+     path('resource_list/', views.resource_list, name='resource_list'),
 ]
