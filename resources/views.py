34f9f200fbe43c85d250381b7a8a17f98b9282a3
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-04 02:15:24 +0000

    new changes

--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import DownloadDocument
+from django.core.paginator import Paginator, EmptyPage
 
 def download_list(request):
     downloads = DownloadDocument.objects.all()
