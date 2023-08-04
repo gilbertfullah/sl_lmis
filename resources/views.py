a3060418453a55f6a58f5b8a6c574f95c0fc641a
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-04 02:06:51 +0000

    new changes

--- resources/views.py
+++ resources/views.py
@@ -3,7 +3,12 @@ from .models import DownloadDocument
 
 def download_list(request):
     downloads = DownloadDocument.objects.all()
+    
+    paginator = Paginator(downloads, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'downloads': downloads,
+        'downloads': page_obj,
     }
     return render(request, 'downloads.html', context)
