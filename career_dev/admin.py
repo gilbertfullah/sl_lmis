786a383cf0d97b09521ac15292de1f288cc1e139
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-04 00:37:46 +0000

    new changes

--- career_dev/admin.py
+++ career_dev/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Institution
 
-# Register your models here.
+class InstitutionAdmin(admin.ModelAdmin):
+    list_filter = ['name']
+    list_display = ['name']
+    list_per_page = 10
+
+admin.site.register(Institution, InstitutionAdmin)
