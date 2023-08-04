d16b19a206672b8d1246a429480334f606957644
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-04 01:31:44 +0000

    new changes

--- news_and_events/urls.py
+++ news_and_events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
      path('', views.news_and_events, name='news_and_events'),
+     path('news/<int:news_id>/', views.news_and_event_detail, name='news_and_event_detail'),
 ]
--- news_and_events/views.py
+++ news_and_events/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from .models import NewsAndEvents
 
 def news_and_events(request):
-
+    news_and_events_list = NewsAndEvents.objects.all()
+    context = {'news_and_events_list': news_and_events_list}
+    
+    
     return render(request, 'news_and_events.html') 
+
+def news_and_event_detail(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    context = {'news': news}
+    return render(request, 'news_and_event_details.html', context)
