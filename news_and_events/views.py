fb43f098eaa048c681d80316f4c4243901a99ab4
Gilbert Fullah <gilbertfullah@gmail.com>
2023-08-04 01:38:05 +0000

    new changes

--- news_and_events/views.py
+++ news_and_events/views.py
@@ -5,8 +5,7 @@ def news_and_events(request):
     news_and_events_list = NewsAndEvents.objects.all()
     context = {'news_and_events_list': news_and_events_list}
     
-    
-    return render(request, 'news_and_events.html') 
+    return render(request, 'news_and_events.html', context) 
 
 def news_and_event_detail(request, news_id):
     news = get_object_or_404(News, pk=news_id)
