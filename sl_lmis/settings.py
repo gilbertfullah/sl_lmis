c0abdf42dd6b154aac3602cebe2b15787524f590
Gilbert Fullah <gilbertfullah@gmail.com>
2023-09-04 23:52:25 +0000

    Changes

--- sl_lmis/settings.py
+++ sl_lmis/settings.py
@@ -226,8 +226,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'gilbertfullah@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yusuf290419'
-DEFAULT_FROM_EMAIL = 'gilbertfullah@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['HOST_PASSWORD'],
+DEFAULT_FROM_EMAIL = os.environ['EMAIL'],
 
 
 LOGIN_URL = 'login'
