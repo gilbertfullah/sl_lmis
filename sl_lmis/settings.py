a8d175ea8c6c8c1a939624b89fcfe87e6e6a090b
Gilbert Fullah <gilbertfullah@gmail.com>
2023-09-05 00:00:24 +0000

    Changes

--- sl_lmis/settings.py
+++ sl_lmis/settings.py
@@ -225,7 +225,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gilbertfullah@gmail.com'
+EMAIL_HOST_USER = os.environ['HOST_USER'],
 EMAIL_HOST_PASSWORD = os.environ['HOST_PASSWORD'],
 DEFAULT_FROM_EMAIL = os.environ['EMAIL'],
 
